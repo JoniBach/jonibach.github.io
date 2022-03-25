db13600f230cbea68747710de8d61012465e94a6
James Crook <48724197+JoniBach@users.noreply.github.com>
2022-03-25 16:24:57 +0000

    eslint

--- src/pages/default/default.tsx
+++ src/pages/default/default.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import { useViewportScroll } from "framer-motion";
