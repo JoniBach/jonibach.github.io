ad3a92aa08b8bc8b8c439aceeec954bce2e4d23e
James Crook <48724197+JoniBach@users.noreply.github.com>
2022-04-09 10:55:08 +0100

    instructions

--- src/components/animatedText/animatedText.tsx
+++ src/components/animatedText/animatedText.tsx
@@ -1,5 +1,6 @@
 import { Text, TextContainer } from "./animatedText-style";
 import ProgressBar from "../progressBar/progressBar";
+import { motion } from "framer-motion";
 
 type AnimatedTextProps = {
   translateWidth: any;
@@ -14,9 +15,31 @@ function AnimatedText({
   data,
   bar,
 }: AnimatedTextProps) {
+  const startFade = translateWidth(0) / 100;
+  const showStart = startFade < 0 ? 0 : startFade;
+
+  const endFade = 0 - translateWidth(data.length - 1) / 100;
+  const showEnd = endFade < 0 ? 0 : endFade;
+
   return (
     <TextContainer>
       <TextContainer>
+        <motion.span
+          animate={{
+            opacity: showStart,
+          }}
+        >
+          scroll to continue
+        </motion.span>
+
+        <motion.span
+          animate={{
+            opacity: showEnd,
+          }}
+        >
+          fin
+        </motion.span>
+
         {data.map((d: any, i: any) => (
           <Text
             animate={{
